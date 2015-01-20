dd2ce16eebb7699db2f6ae2a344b45766932f682
Ian W. Atha <ian@gumroad.com>
2015-01-19 20:05:30 -0800

    Initial Commit

---
+++ stargate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  stargate
+//
+//  Created by Ian Atha on 1/19/15.
+//  Copyright (c) 2015 Ian Atha. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
